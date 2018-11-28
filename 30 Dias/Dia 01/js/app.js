849eb94e5b97f721fabf4e955ceff4d0ba2da058
Vanessa Ribeiro <Van Ribeiro>
2018-11-28 18:46:47 -0200

    Modificações no design.

--- 30 Dias/Dia 01/js/app.js
+++ 30 Dias/Dia 01/js/app.js
@@ -1,9 +1,22 @@
+'use strict';
+
 jQuery(function () {
  jQuery(window).scroll(function () {
-  if (jQuery(this).scrollTop() > 400) {
-   $("#menu").css('background-color', '#1a1a1a');
+  if (jQuery(this).scrollTop() > 300) {
+    $("#menu").css({
+        "background-color": "#252525",
+    });
+    $("#menu a").css({
+        "color" : "#f5f5f5"
+    });
   } else {
-   $("#menu").css('background-color', '#1a1a1a00');
+    $(".menu-top").css({
+        "background-color" : "#1a1a1a00"
+    });
+
+    $("#menu a").css({
+        "color" : "#1a1a1a"
+    });
   }
  });
 });

f498cc260d789cfd6636d987c121ab33485fd238
Vanessa Ribeiro <Van Ribeiro>
2018-12-05 03:16:48 -0200

    Desafio concluído.Ainda com problemas no menu responsivo.

---
+++ 30 Dias/Dias/Dia 02/js/app.js
@@ -0,0 +1,8 @@
+function rwdMenu() {
+    var x = document.getElementById("all-nav-links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
